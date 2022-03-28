36ac29a0fda116430d4c2e4c533b48ccd1eeae15
haroonrashid72 <haroon.rashid@swipbox.com>
2022-03-28 06:23:06 +0500

    View Added

---
+++ db/quran_view.sql
@@ -0,0 +1,22 @@
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `newschema`.`quran_view` AS
+    SELECT 
+        `newschema`.`quran_detail`.`id` AS `id`,
+        `newschema`.`quran`.`english` AS `english`,
+        `newschema`.`quran`.`token` AS `token`,
+        `newschema`.`quran`.`verse` AS `verse`,
+        `newschema`.`quran`.`chapter` AS `chapter`,
+        `newschema`.`quran`.`root_word` AS `root_word`,
+        `newschema`.`quran`.`buckwalter` AS `buckwalter`,
+        `newschema`.`quran_detail`.`type` AS `type`,
+        `newschema`.`quran_detail`.`type_detail_v2` AS `type_detail`,
+        `newschema`.`verse`.`verse_text` AS `verse_text`
+    FROM
+        ((`newschema`.`quran`
+        LEFT JOIN `newschema`.`quran_detail` ON (`newschema`.`quran`.`id` = `newschema`.`quran_detail`.`quran_id`))
+        LEFT JOIN `newschema`.`verse` ON (`newschema`.`quran`.`verse` = `newschema`.`verse`.`verse`
+            AND `newschema`.`quran`.`chapter` = `newschema`.`verse`.`chapter`))
+    ORDER BY `newschema`.`quran`.`chapter` , `newschema`.`quran`.`verse` , `newschema`.`quran`.`token`
